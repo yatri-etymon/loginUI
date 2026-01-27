014699f7f7a8919042176f19f2355484bf5a3101
Yatri Parekh <yatri@etymon.in>
2026-01-27 18:53:35 +0530

    start screen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,11 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:login_demo/widgeta/background_image.dart';
+import 'package:login_demo/widgeta/square_tile.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [BackgroundImage(), Scaffold(backgroundColor: Colors.transparent,)]);
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16.0,
+                  left: 16.0,
+                  right: 16.0,
+                  bottom: 50.0,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome',
+                      style: GoogleFonts.lato(
+                        letterSpacing: 1,
+                        fontSize: 27,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.white),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Login with OTP',
+                        style: GoogleFonts.lato(
+                          letterSpacing: 2,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.teal.shade700,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(indent: 75, color: Colors.white),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              "Or",
+                              style: GoogleFonts.lato(
+                                letterSpacing: 2,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(endIndent: 75, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SquareTile(
+                          imagePath: 'assets/icons/google.png',
+                          onTap: () {},
+                        ),
+                        SizedBox(width: 20),
+                        SquareTile(
+                          imagePath: 'assets/icons/apple.png',
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/widgeta/background_image.dart
+++ lib/widgeta/background_image.dart
@@ -5,12 +5,24 @@ class BackgroundImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/image/teal_bg.jpg'),
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(Colors.black12, BlendMode.darken),
+    return ShaderMask(
+      shaderCallback: (bounds) {
+        return const LinearGradient(
+          begin: Alignment.center,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 180, 240, 233),
+            Color.fromARGB(255, 28, 233, 206),
+          ],
+        ).createShader(bounds);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image/image.png'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black12, BlendMode.darken),
+          ),
         ),
       ),
     );
